952aa5cafb6282b111f3855728d4b3464a66c9f5
George Calin <george.calin@lacon.de>
2019-10-14 15:45:26 +0300

    Add all files.

---
+++ PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using PersonRepository.Interface;
+using PersonRepository.Service;
+using System.Windows;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ServiceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+            IPersonRepository repository = new ServiceRepository();
+            var persons = repository.GetPeople();
+            foreach (var person in persons)
+            {
+                PersonListBox.Items.Add(person);
+            }
+            ShowRepositoryType(repository);
+        }
+
+        private void CSVFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SQLFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            PersonListBox.Items.Clear();
+            RepositoryTypeTextBlock.Text = string.Empty;
+        }
+
+        private void ShowRepositoryType(IPersonRepository repository)
+        {
+            RepositoryTypeTextBlock.Text = repository.GetType().ToString();
+        }
+    }
+}
---
+++ PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using PersonRepository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository : IPersonRepository
+    {
+
+        WebClient webClient = new WebClient();
+        string baseUri = "http://localhost:9874/api/people"; 
+
+        public IEnumerable<Person> GetPeople()
+        {
+            string result = webClient.DownloadString(baseUri);
+            IEnumerable<Person> people = JsonConvert.DeserializeObject<IEnumerable<Person>>(result);
+            return people;
+        }
+
+        public Person GetPerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPerson(Person newPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePerson(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    
+
+        public void UpdatePerson(int id, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
